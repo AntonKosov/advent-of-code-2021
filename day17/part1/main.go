2ffa7f94dffffdeff0b573bf6fe1db09cc1978ae
Anton Kosov <AntonRKosov@gmail.com>
2021-12-17 07:29:38 -0600

    day17

---
+++ day17/part1/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+
+	"github.com/AntonKosov/advent-of-code-2021/aoc"
+)
+
+func main() {
+	data := read()
+	r := process(data)
+	fmt.Printf("Answer: %v\n", r)
+}
+
+type target struct {
+	min aoc.Vector2
+	max aoc.Vector2
+}
+
+func read() (data target) {
+	line := aoc.ReadAllInput()[0]
+
+	s := strings.Split(line[15:], ", y=")
+	xs := strings.Split(s[0], "..")
+	ys := strings.Split(s[1], "..")
+
+	data.min.X = aoc.StrToInt(xs[0])
+	data.min.Y = aoc.StrToInt(ys[1])
+	data.max.X = aoc.StrToInt(xs[1])
+	data.max.Y = aoc.StrToInt(ys[0])
+
+	return data
+}
+
+func process(data target) int {
+	minXVelocity := getMinVelocity(data.min.X)
+	max := 0
+	const maxVelocity = 200
+	for startXV := minXVelocity; startXV < data.max.X; startXV++ {
+		xMax := 0
+		for startYV := data.max.Y; startYV <= maxVelocity; startYV++ {
+			p := aoc.NewVector2(startXV, startYV)
+			reachTarget := false
+			xyMax := 0
+			xv := startXV
+			yv := startYV
+			for p.X <= data.max.X && p.Y >= data.max.Y {
+				if p.X >= data.min.X && p.Y <= data.min.Y {
+					reachTarget = true
+				}
+				if xv == 0 && (p.X < data.min.X || p.X > data.max.X) {
+					break
+				}
+				xv = aoc.Max(0, xv-1)
+				yv -= 1
+				p.X += xv
+				p.Y += yv
+				xyMax = aoc.Max(xyMax, p.Y)
+			}
+			if reachTarget {
+				xMax = aoc.Max(xMax, xyMax)
+			}
+		}
+		max = aoc.Max(max, xMax)
+	}
+
+	return max
+}
+
+func getMinVelocity(distance int) int {
+	return (-1 + int(math.Sqrt(float64(1+8*distance)))) / 2
+}
